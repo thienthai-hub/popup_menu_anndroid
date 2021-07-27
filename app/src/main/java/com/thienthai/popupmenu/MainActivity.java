f882622d94c6580f5009305ddaff3943c98682e8
thienthai-hub <vohuyenthienthai94@gmail.com>
2021-07-27 19:44:06 +0700

    Popup menu

--- app/src/main/java/com/thienthai/popupmenu/MainActivity.java
+++ app/src/main/java/com/thienthai/popupmenu/MainActivity.java
@@ -14,33 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnPopup = (Button) findViewById(R.id.button);
-        btnPopup.setOnClickListener(new View.OnClickListener() {
+            btnPopup = (Button) findViewById(R.id.button);
+            btnPopup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PopupMenu();
+                    PopupMenu();
             }
         });
     }
-    private void PopupMenu() {
-        PopupMenu popupMenu = new PopupMenu(this, btnPopup);
-        popupMenu.getMenuInflater().inflate(R.menu.menu, popupMenu.getMenu());
-        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.angular: btnPopup.setText("Angrular");
-                        break;
-                    case R.id.Html: btnPopup.setText("Html");
-                        break;
-                    case R.id.java: btnPopup.setText("Java");
-                        break;
-                    case R.id.Php: btnPopup.setText("Php");
-                        break;
+        private void PopupMenu() {
+            PopupMenu popupMenu = new PopupMenu(this, btnPopup);
+            popupMenu.getMenuInflater().inflate(R.menu.menu, popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    switch (item.getItemId()){
+                        case R.id.angular: btnPopup.setText("Angrular");
+                            break;
+                        case R.id.Html: btnPopup.setText("Html");
+                            break;
+                        case R.id.java: btnPopup.setText("Java");
+                            break;
+                        case R.id.Php: btnPopup.setText("Php");
+                            break;
+                    }
+                    return false;
                 }
-                return false;
-            }
-        });
-        popupMenu.show();
-    }
+            });
+            popupMenu.show();
+        }
 }
